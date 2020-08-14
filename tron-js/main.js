1758a553314eb94619a582733920c29eece9d1b8
Yehor Bublyk <yehor.bk@gmail.com>
2020-08-15 00:45:47 +0300

    tron: player movings added

--- tron-js/main.js
+++ tron-js/main.js
@@ -17,6 +17,7 @@ const bluePlayer = {
 	posX: -1,
 	posY: -1,
 	speed: 1,
+	direction: directionState.UP,
 };
 
 const orangePlayer = {
@@ -26,7 +27,12 @@ const orangePlayer = {
 	posX: -1,
 	posY: -1,
 	speed: 1,
+	direction: directionState.DOWN,
 }
+
+const clearField = () => {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+};
 
 const prepareBluePlayer = () => {
   bluePlayer.posX = canvas.width / 2 - bluePlayer.width / 2;
@@ -43,30 +49,87 @@ const draw = object => {
 	context.fillRect(object.posX, object.posY, object.width, object.height);
 };
 
+const move = player => {
+  if (player.direction === directionState.LEFT &&
+		player.posX > 0) {
+		player.posX -= player.speed;
+	} else if (player.direction === directionState.RIGHT &&
+		(player.posX + player.width) < canvas.width) {
+		player.posX += player.speed;
+	}
+	if (player.direction === directionState.UP &&
+		player.posY > 0) {
+		player.posY -= player.speed;
+	} else if (player.direction === directionState.DOWN &&
+		(player.posY + player.height) < canvas.height) {
+		player.posY += player.speed;
+	}
+};
+
+const changeDirection = (player, direction) => {
+  switch (direction) {
+    case directionState.LEFT:
+      if (player.direction !== directionState.RIGHT) {
+        player.direction = direction;
+      }
+      break;
+    case directionState.UP:
+      if (player.direction !== directionState.DOWN) {
+        player.direction = direction;
+      }
+      break;
+    case directionState.RIGHT:
+      if (player.direction !== directionState.LEFT) {
+        player.direction = direction;
+      }
+      break;
+    case directionState.DOWN:
+      if (player.direction !== directionState.UP) {
+        player.direction = direction;
+      }
+      break;
+  }
+};
+
 const keyEventListener = event => {
   switch (event.keyCode) {
     case 37: // left
-      changeDirection(directionState.LEFT);
+      changeDirection(bluePlayer, directionState.LEFT);
       break;
     case 38: // up
-      changeDirection(directionState.UP);
+      changeDirection(bluePlayer, directionState.UP);
       break;
     case 39: // right
-      changeDirection(directionState.RIGHT);
+      changeDirection(bluePlayer, directionState.RIGHT);
       break;
     case 40: // down
-      changeDirection(directionState.DOWN);
+      changeDirection(bluePlayer, directionState.DOWN);
+      break;
+		case 65: // a
+      changeDirection(orangePlayer, directionState.LEFT);
+      break;
+    case 87: // w
+      changeDirection(orangePlayer, directionState.UP);
+      break;
+    case 68: // d
+      changeDirection(orangePlayer, directionState.RIGHT);
+      break;
+    case 83: // s
+      changeDirection(orangePlayer, directionState.DOWN);
       break;
   }
 };
 
 const reloadFrame = () => {
+  clearField();
   draw(bluePlayer);
 	draw(orangePlayer);
+	move(bluePlayer);
+	move(orangePlayer);
 };
 
 prepareBluePlayer();
 prepareOrangePlayer();
 
-setTimeout(reloadFrame, 10);
+setInterval(reloadFrame, 10);
 addEventListener('keydown', keyEventListener);
