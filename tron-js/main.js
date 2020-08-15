c9c709a1c742b10607498f08a526eeaf8601667e
Yehor Bublyk <yehor.bk@gmail.com>
2020-08-15 11:03:46 +0300

    tron: basic functionality finished, special abilities added

--- tron-js/main.js
+++ tron-js/main.js
@@ -12,58 +12,128 @@ const directionState = {
 
 const bluePlayer = {
   color: '#98d4da',
-	width: 16,
-	height: 16,
+	width: 8,
+	height: 8,
 	posX: -1,
 	posY: -1,
-	speed: 1,
 	direction: directionState.UP,
+	speed: 0,
+	barrier: false,
 };
 
 const orangePlayer = {
   color: '#efe059',
-	width: 16,
-	height: 16,
+	width: 8,
+	height: 8,
 	posX: -1,
 	posY: -1,
-	speed: 1,
 	direction: directionState.DOWN,
-}
+	speed: 0,
+	barrier: false,
+};
+
+const barrierList = [];
 
 const clearField = () => {
   context.clearRect(0, 0, canvas.width, canvas.height);
 };
 
 const prepareBluePlayer = () => {
-  bluePlayer.posX = canvas.width / 2 - bluePlayer.width / 2;
+	bluePlayer.posX = canvas.width / 2 - bluePlayer.width / 2;
 	bluePlayer.posY = canvas.height - bluePlayer.height * 2;
+	bluePlayer.direction = directionState.UP;
+	bluePlayer.speed = 1;
+	bluePlayer.barrier = true;
 };
 
 const prepareOrangePlayer = () => {
+	orangePlayer.direction = directionState.DOWN;
 	orangePlayer.posX = canvas.width / 2 - orangePlayer.width / 2;
 	orangePlayer.posY = 0 + orangePlayer.height * 2;
+	orangePlayer.speed = 1;
+	orangePlayer.barrier = true;
 };
 
-const draw = object => {
-  context.fillStyle = object.color;
-	context.fillRect(object.posX, object.posY, object.width, object.height);
+const loadGame = () => {
+	barrierList.splice(0, barrierList.length);
+	clearField();
+	prepareBluePlayer();
+	prepareOrangePlayer();
+};
+
+const gameOver = () => {
+	// alert('Game Over!');
+};
+
+const generateBarrierUnit = player => {
+	if (player.barrier) {
+		const posX = player.posX + player.width / 2 - 2;
+		const posY = player.posY + player.height / 2 - 2;
+  	const barrierUnit = {
+			posX,
+			posY,
+			width: 4,
+			height: 4,
+			color: player.color,
+		};
+		barrierList.push(barrierUnit);
+	}
+};
+
+const spawnBarrier = () => {
+  for (const barrierUnit of barrierList) {
+		draw(barrierUnit)
+	};
+};
+
+const collisionRecognizer = player => {
+  const borderCollisionX = (player.posX <= 0 ||
+	  (player.posX + player.width) >= canvas.width);
+	const borderCollisionY = (player.posY <= 0 ||
+		(player.posY + player.height) >= canvas.height);
+	const barrierCollision = (() => {
+  	for (const barrierUnit of barrierList) {
+			const innerBarrierCollisionX = (player.posX === barrierUnit.posX) &&
+				((player.posX + player.width) === (barrierUnit.posX + barrierUnit.width));
+			const innerBarrierCollisionY = (player.posY === barrierUnit.posY) &&
+				((player.posY + player.height) === (barrierUnit.posY + barrierUnit.height));
+			if (innerBarrierCollisionX && innerBarrierCollisionY) {
+				return false;
+			}
+		  const barrierCollisionX = (player.posX >= barrierUnit.posX &&
+        player.posX <= (barrierUnit.posX + barrierUnit.width));
+      const barrierCollisionY = (player.posY <=
+        (barrierUnit.posY + barrierUnit.height)) &&
+        (player.posY + player.height) >= barrierUnit.posY;
+      if (barrierCollisionX && barrierCollisionY) {
+				console.log(player);
+				console.log(barrierUnit);
+				//alert();
+        return false; // true
+      }
+		}
+		return false;
+	})();
+	if (borderCollisionX || borderCollisionY ||
+		barrierCollision) {
+		gameOver();
+		loadGame();
+	}
 };
 
 const move = player => {
-  if (player.direction === directionState.LEFT &&
-		player.posX > 0) {
+	generateBarrierUnit(player);
+	if (player.direction === directionState.LEFT) {
 		player.posX -= player.speed;
-	} else if (player.direction === directionState.RIGHT &&
-		(player.posX + player.width) < canvas.width) {
+	} else if (player.direction === directionState.RIGHT) {
 		player.posX += player.speed;
 	}
-	if (player.direction === directionState.UP &&
-		player.posY > 0) {
+	if (player.direction === directionState.UP) {
 		player.posY -= player.speed;
-	} else if (player.direction === directionState.DOWN &&
-		(player.posY + player.height) < canvas.height) {
+	} else if (player.direction === directionState.DOWN) {
 		player.posY += player.speed;
-	}
+	}	
+	collisionRecognizer(player);
 };
 
 const changeDirection = (player, direction) => {
@@ -91,6 +161,14 @@ const changeDirection = (player, direction) => {
   }
 };
 
+const switchSpeed = player => {
+	player.speed = player.speed === 1 ? 2 : 1;
+};
+
+const switchBarrier = player => {
+	player.barrier = !player.barrier;
+};
+
 const keyEventListener = event => {
   switch (event.keyCode) {
     case 37: // left
@@ -105,6 +183,12 @@ const keyEventListener = event => {
     case 40: // down
       changeDirection(bluePlayer, directionState.DOWN);
       break;
+		case 78: // n
+		  switchSpeed(bluePlayer);
+			break;
+		case 77: // m
+			switchBarrier(bluePlayer);
+			break;
 		case 65: // a
       changeDirection(orangePlayer, directionState.LEFT);
       break;
@@ -117,7 +201,18 @@ const keyEventListener = event => {
     case 83: // s
       changeDirection(orangePlayer, directionState.DOWN);
       break;
+		case 88: // x
+		  switchSpeed(orangePlayer);
+			break;
+		case 67: // c
+			switchBarrier(orangePlayer);
+			break;
   }
+};
+
+const draw = object => {
+  context.fillStyle = object.color;
+	context.fillRect(object.posX, object.posY, object.width, object.height);
 };
 
 const reloadFrame = () => {
@@ -126,10 +221,9 @@ const reloadFrame = () => {
 	draw(orangePlayer);
 	move(bluePlayer);
 	move(orangePlayer);
+	spawnBarrier();
 };
 
-prepareBluePlayer();
-prepareOrangePlayer();
-
+loadGame();
 setInterval(reloadFrame, 10);
 addEventListener('keydown', keyEventListener);
